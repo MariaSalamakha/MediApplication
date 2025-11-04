a20cc2a05ea2131120f86c2e20de178839679060
MariaSalamakha <maria.salamakha@gmail.com>
2025-11-04 20:44:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+gsap.to("#textMain", {
+    text: "Let's relax and move forward ",
+    duration: 2,
+    repeat: -1,
+    repeatDelay: .9,
+    ease: "power1.in",
+    yoyo: true,
+    delay: .5
+})
